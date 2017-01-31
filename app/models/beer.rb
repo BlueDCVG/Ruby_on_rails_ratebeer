24c2653673bff8c8b1ab8fcca5441e9a394d588c
Toni Tykka <toni.tykka@helsinki.fi>
2017-01-31 19:28:05 +0200

    Week 2 ready

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,11 +1,8 @@
 class Beer < ActiveRecord::Base
-  belongs_to :brewery
-  has_many :ratings
+  include RatingAverage
 
-  def average_rating
-    return 0 if ratings.empty?
-    ratings.map{|r| r.score }.sum/ratings.count
-  end
+  belongs_to :brewery
+  has_many :ratings, dependent: :destroy
 
   def to_s
     "#{name}, #{brewery.name}"
