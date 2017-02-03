9d83bf4380c00726bd945183f16c5214751d5170
Toni Tykka <toni.tykka@helsinki.fi>
2017-02-04 00:11:44 +0200

    Week 3

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ActiveRecord::Base
+  include RatingAverage
+
+  has_many :ratings
+  has_many :beers, through: :ratings
+  has_many :memberships
+  has_many :beer_clubs, through: :memberships
+
+  validates :username, uniqueness: true,
+                       length: { minimum: 3,
+                                 maximum: 30}
+
+  has_secure_password
+
+
+end
